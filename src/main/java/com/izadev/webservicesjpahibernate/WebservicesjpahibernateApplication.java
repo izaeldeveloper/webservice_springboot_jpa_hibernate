472db31afee7bfdb186e5e69441e973fc679d6c2
Izael Araujo <izael.developer@proton.me>
2025-03-24 19:26:22 -0300

    Projeto criado

---
+++ src/main/java/com/izadev/webservicesjpahibernate/WebservicesjpahibernateApplication.java
@@ -0,0 +1,13 @@
+package com.izadev.webservicesjpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesjpahibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesjpahibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/izadev/webservicesjpahibernate/WebservicesjpahibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izadev.webservicesjpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesjpahibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
