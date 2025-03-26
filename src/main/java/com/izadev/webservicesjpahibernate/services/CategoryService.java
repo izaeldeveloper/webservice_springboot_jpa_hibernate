31e556e408aa6357f10bf4b8bafa5ac41a1cd67d
Izael Araujo <izael.developer@proton.me>
2025-03-26 08:20:03 -0300

    Entidade category

---
+++ src/main/java/com/izadev/webservicesjpahibernate/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.izadev.webservicesjpahibernate.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.izadev.webservicesjpahibernate.entities.Category;
+import com.izadev.webservicesjpahibernate.repositories.CategoryRepository;
+
+// @Component
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
